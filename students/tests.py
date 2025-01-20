9361218025110668c0c042c4cbf5f6e2c249eb78
Srijon23 <srijonchowdhury123@gmail.com>
2025-01-20 18:29:15 +0600

    complete

--- students/tests.py
+++ students/tests.py
@@ -7,9 +7,9 @@ class StudentModelUnitTestCase(TestCase):
     def setUp(self):
         self.student = Student.objects.create(
             student_number=random.randint(10000, 99999),
-            first_name='Bob',
-            last_name='Smith',
-            email='bob.smith@test.com',
+            first_name='Srijon',
+            last_name='Chowdhury',
+            email='srijon@test.com',
             field_of_study='Computer Science',
             gpa=3.92
         )
